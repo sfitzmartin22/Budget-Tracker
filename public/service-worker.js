c9732048568bd53b525a7e123e5d51baf5f7ce02
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-05-27 18:21:20 -0400

    uncommenting service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,60 +1,60 @@
-// const FILES_TO_CACHE = [
-//     "/",
-//     "/index.html",
-//     "/styles.css",
-//     "/db.js",
-//     "/index.js",
-//     "/manifest.webmanifest",
-//     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-//     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-// ];
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/db.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 
-// const PRECACHE = "precache-v1";
-// const RUNTIME = "runtime";
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
-// self.addEventListener("install", (event) => {
-//     event.waitUntil(
-//         caches.open(PRECACHE)
-//             .then((cache) => cache.addAll(FILES_TO_CACHE))
-//             .then(self.skipWaiting())
-//     );
-// });
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
 
-// self.addEventListener("activate", (event) => {
-//     const currentCaches = [PRECACHE, RUNTIME];
-//     event.waitUntil(
-//         caches
-//         .keys()
-//         .then((cacheNames) => {
-//             return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-//     })
-//     .then((cachesToDelete) => {
-//         return Promise.all(
-//             cachesToDelete.map((cachesToDelete) => {
-//                 return caches.delete(cachesToDelete);
-//             })
-//         );
-//     })
-//     .then(() => self.clients.claim())
-//     );
-// });
+self.addEventListener("activate", (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+        .keys()
+        .then((cacheNames) => {
+            return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+    })
+    .then((cachesToDelete) => {
+        return Promise.all(
+            cachesToDelete.map((cachesToDelete) => {
+                return caches.delete(cachesToDelete);
+            })
+        );
+    })
+    .then(() => self.clients.claim())
+    );
+});
 
-// self.addEventListener('fetch', (event) => {
-//     if (event.request.url.startsWith(self.location.origin)) {
-//         event.respondWith(
-//             caches.match(event.request).then((cachedResponse) => {
-//                 if (cachedResponse) {
-//                     return cachedResponse;
-//                 }
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
 
-//                 return caches.open(RUNTIME).then((cache) => {
-//                     return fetch(event.request).then((response) => {
-//                         return cache.put(event.request, response.clone()).then(() => {
-//                             return response;
-//                         });
-//                     });
-//                 });
-//             })
-//         );
-//     }
-// });
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
