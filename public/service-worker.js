493b32196a4cff81133da6e4ba8e58379edafc58
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-05-26 23:25:58 -0400

    added service worker and am working through debugging, application is currently a pwa and is downoadable however it appears I lost some functionality and need to debug through code

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,17 +3,57 @@ const staticBudgetToCache = [
     "/index.html",
     "/styles.css",
     "/db.js",
-    "/index.js"
+    "/index.js",
+    "/manifest.webmanifest",
+
 ];
 
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches
-            .open(PRECACHE)
+self.addEventListener("install", (installEvent) => {
+    installEvent.waitUntil(
+        caches.open(PRECACHE)
             .then((cache) => cache.addAll(staticBudgetToCache))
             .then(self.skipWaiting())
     );
+});
+
+self.addEventListener("activate", (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+        .keys()
+        .then((cacheNames) => {
+            return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+    })
+    .then((cachesToDelete) => {
+        return Promise.all(
+            cachesToDelete.map((cachesToDelete) => {
+                return caches.delete(cachesToDelete);
+            })
+        );
+    })
+    .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (fetchEvent) => {
+    if (fetchEvent.request.url.startsWith(self.location.origin)) {
+        fetchEvent.respondWith(
+            caches.match(fetchEvent.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(fetchEvent.request).then((response) => {
+                        return cache.put(fetchEvent.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });
