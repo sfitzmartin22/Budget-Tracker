c9ff19e89c3f1b4708d2fa95e7fd71b0b2b8d473
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-05-29 15:20:36 -0400

    webmanifest page completed, just need to run it

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const WebpackManifest = requirerequire('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: "./publiic/index.js",
+    output: {
+        path: __dirname + "/public/dist",
+        filename: "bundle.js"
+    },
+
+    mode: "production",
+    plugins: [
+    new WebpackPwaManifest({
+      
+      filename: "manifest.json",
+      inject: false,
+      fingerprints: false,
+
+      name: "Budget Tracker",
+      short_name: "Budget App",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      start_url: "/",
+      display: "standalone",
+
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            "public/icons/icon-512x512.png"
+            ),
+          // the plugin will generate an image for each size
+          // included in the size array
+          size: [192, 512]
+        }
+      ]
+    })
+  ]
+};
+
+module.exports = config;
