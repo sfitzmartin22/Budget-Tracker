9c99b03068867c24ad283eb4be13771a9b5c036a
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-05-29 16:33:15 -0400

    even more heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useFindAndModify: false,
+  useCreateIndex: true,
   useUnifiedTopology: true
 });
 
